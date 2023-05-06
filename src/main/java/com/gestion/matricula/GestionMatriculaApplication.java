e6b79e6f33df1b0bba4d22b6dd4fbb56f5645792
flaguna <flaguna@sintad.pe>
2023-05-05 22:01:55 -0500

    proyecto base

---
+++ src/main/java/com/gestion/matricula/GestionMatriculaApplication.java
@@ -0,0 +1,12 @@
+package com.gestion.matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMatriculaApplication {
+
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(GestionMatriculaApplication.class, args);
+	}
+}
